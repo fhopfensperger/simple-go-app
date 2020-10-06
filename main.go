b80ea1ec59ed32f3908cd671d3117cd4c8947b2e
Florian Hopfensperger <fhopfensperger@gmail.com>
2020-10-06 12:57:35 +0200

    Add version

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 
 const port = "8080"
 
+var version = "v0.0.1"
+
 func main() {
 	http.HandleFunc("/hello", helloWorldHandler)
-	fmt.Printf("Running on %v architecture\n", runtime.GOARCH)
+	fmt.Printf("Running on %v architecture, app version %v\n", runtime.GOARCH, version)
 	fmt.Println("Listening on port", port)
 	http.ListenAndServe(":"+port, nil)
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Received incoming web request %v on path %v\n", r.Method, r.URL.Path)
-	w.Write([]byte("Hello"))
+	w.Write([]byte("Hello version: " + version))
 }
